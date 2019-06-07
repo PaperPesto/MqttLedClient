8baab0a848dd4c4a2904e4f49849c75aaa23ea66
Leonardo Bianchini <leonardo.bianchini90@gmail.com>
2019-06-07 07:59:51 +0200

    Rimosso JsonBuffer. Adesso il client non è più configurato in JSON

--- MqttLedClient.ino
+++ MqttLedClient.ino
@@ -1,6 +1,5 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
-#include <ArduinoJson.h>
 #include "LedArray.h"
 #include "Led.h"
 
@@ -16,19 +15,19 @@
 #define LEDS2 12
 
 // Configurazione Wifi
-const char* ssid = "***";
-const char* password =  "***";
+const char *ssid = "***";
+const char *password = "***";
 //IPAddress ip(192, 168, 1, 50);
 IPAddress ip;
 
 // Configurazione MQTT
-const char* mqttServer = "***";
+const char *mqttServer = "***";
 const int mqttPort = 000;
-const char* mqttUser = "***";
-const char* mqttPassword = "***";
+const char *mqttUser = "***";
+const char *mqttPassword = "***";
 
 // Ledarray
-LedArray ledarray (LED0, LED1, LED2, LED3, LED4);
+LedArray ledarray(LED0, LED1, LED2, LED3, LED4);
 
 // LedNetworkStatus
 Led led_wifi_status = Led(LEDS1);
@@ -36,37 +35,33 @@ Led led_broker_status = Led(LEDS2);
 
 WiFiClient espClient;
 PubSubClient mqttClient(espClient);
- 
-void setup() {
- 
+
+void setup()
+{
+
   Serial.begin(115200);
 
   connectToWifi();
-  
+
   refreshLedStatus();
 
-  while (!mqttClient.connected()) {
+  while (!mqttClient.connected())
+  {
     connectToBroker();
   }
 
   refreshLedStatus();
 }
- 
-void callback(char* topic, byte* payload, unsigned int length) {
-  
-  // 1 - parser topic. in base al topic posso scegliere più strade
-  // (char* inputtopic, char* matchtopic, function callback_func) => { if(matchtopic == inputtopic) callback_func(); }        
-  // 2 - json parser del payload (all'interno della stessa funzionalità definita dal topic)
-  // 
-  // 3 - Nel caso della funzionalità classica (probabilmente l'unica), estrarre i valori int dal json e darli in pasto al LedArray (che funge da controller)
- 
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+
   Serial.print("Message arrived in topic: ");
   Serial.println(topic);
 
-  StaticJsonBuffer<200> jsonBuffer;
- 
   Serial.println("Message:");
-  for (int i = 0; i < length; i++) {
+  for (int i = 0; i < length; i++)
+  {
     Serial.print((char)payload[i], BIN);
     Serial.print(" - ");
     Serial.println((char)payload[i], HEX);
@@ -75,98 +70,96 @@ void callback(char* topic, byte* payload, unsigned int length) {
   // Creazione stringa ---
   // alla fine della stringa devo avere un \0 per terminare la stringa
   // altrimenti println() continua a leggere finché non trova '\0'
-  char* bufferString;
-  bufferString = (char*) malloc (length + 1);
+  char *bufferString;
+  bufferString = (char *)malloc(length + 1);
   Serial.print("Indirizzo bufferString: ");
   Serial.println(*bufferString, HEX);
-  for (int i = 0; i < length; i++) {
+  for (int i = 0; i < length; i++)
+  {
     bufferString[i] = (char)payload[i];
   }
   bufferString[length] = '\0';
   Serial.print("Buffer string: ");
   Serial.println(bufferString);
-  
-  // Conversione stringa -> json ---
-  JsonObject& root = jsonBuffer.parseObject(bufferString);
-  // Test if parsing succeeds.
-  if (!root.success()) {
-    Serial.println("parseObject() failed");
-  }
-  else {
-    Serial.println("parseObject() success");
 
-    const float led0 = root["led0"];
-    const float led1 = root["led1"];
-    const float led2 = root["led2"];
-    const float led3 = root["led3"];
-    const float led4 = root["led4"];
+  const float led0 = 10.5;
+  const float led1 = 20;
+  const float led2 = 30;
+  const float led3 = 50;
+  const float led4 = 70;
 
-    // Print values
-    Serial.println("led0: " + String(led0));
-    Serial.println("led1: " + String(led1));
-    Serial.println("led2: " + String(led2));
-    Serial.println("led3: " + String(led3));
-    Serial.println("led4: " + String(led4));
+  // Print values
+  Serial.println("led0: " + String(led0));
+  Serial.println("led1: " + String(led1));
+  Serial.println("led2: " + String(led2));
+  Serial.println("led3: " + String(led3));
+  Serial.println("led4: " + String(led4));
 
-    // Conversion from percentage to 0:1023
-    int cled0 = map(led0, 0, 100, 0, 1023);
-    int cled1 = map(led1, 0, 100, 0, 1023);
-    int cled2 = map(led2, 0, 100, 0, 1023);
-    int cled3 = map(led3, 0, 100, 0, 1023);
-    int cled4 = map(led4, 0, 100, 0, 1023);
+  // Conversion from percentage to 0:1023
+  int cled0 = map(led0, 0, 100, 0, 1023);
+  int cled1 = map(led1, 0, 100, 0, 1023);
+  int cled2 = map(led2, 0, 100, 0, 1023);
+  int cled3 = map(led3, 0, 100, 0, 1023);
+  int cled4 = map(led4, 0, 100, 0, 1023);
 
-    // Print converted values
-    Serial.println("Converted values");
-    Serial.println("led0: " + String(cled0));
-    Serial.println("led1: " + String(cled1));
-    Serial.println("led2: " + String(cled2));
-    Serial.println("led3: " + String(cled3));
-    Serial.println("led4: " + String(cled4));
+  // Print converted values
+  Serial.println("Converted values");
+  Serial.println("led0: " + String(cled0));
+  Serial.println("led1: " + String(cled1));
+  Serial.println("led2: " + String(cled2));
+  Serial.println("led3: " + String(cled3));
+  Serial.println("led4: " + String(cled4));
 
-    // Sending to LedArray
-    ledarray.Pwm(cled0, cled1, cled2, cled3, cled4);
-  }
-  free(bufferString);
-  
+  // Sending to LedArray
+  ledarray.Pwm(cled0, cled1, cled2, cled3, cled4);
+
   Serial.println("-----------------------");
 }
 
 // Refresha le lucette delle connessioni
-void refreshLedStatus(){
-  if(WiFi.status() != WL_CONNECTED){
+void refreshLedStatus()
+{
+  if (WiFi.status() != WL_CONNECTED)
+  {
     Serial.println("WiFi is off");
     led_wifi_status.Off();
   }
-    else {
-      Serial.println("Wifi is On");
-      led_wifi_status.On();
-    }
+  else
+  {
+    Serial.println("Wifi is On");
+    led_wifi_status.On();
+  }
 
-  if(mqttClient.state() != MQTT_CONNECTED)
+  if (mqttClient.state() != MQTT_CONNECTED)
   {
     Serial.println("mqtt is off");
     led_broker_status.Off();
   }
-    else{
-      Serial.println("mqtt is on");
-      led_broker_status.On();
-    }
+  else
+  {
+    Serial.println("mqtt is on");
+    led_broker_status.On();
+  }
 }
 
 // Connessione broker
-void connectToBroker(){
+void connectToBroker()
+{
 
   mqttClient.setServer(mqttServer, mqttPort);
   mqttClient.setCallback(callback);
-  
+
   Serial.println("Connecting to MQTT broker...");
-  if (mqttClient.connect("ESP8266Client", mqttUser, mqttPassword )) {
+  if (mqttClient.connect("ESP8266Client", mqttUser, mqttPassword))
+  {
 
     Serial.println("connected");
-    
+
     mqttClient.publish("esp/test", "Hello from ESP8266");
     mqttClient.subscribe("esp/test");
-  } else {
+  }
+  else
+  {
 
     Serial.print("failed with state ");
     Serial.print(mqttClient.state());
@@ -175,11 +168,13 @@ void connectToBroker(){
 }
 
 // Connessione WiFi
-void connectToWifi(){
+void connectToWifi()
+{
   //WiFi.config(ip);  // Indirizzo ip statico (commentare se DHCP)
   WiFi.begin(ssid, password);
- 
-  while (WiFi.status() != WL_CONNECTED) {
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.println("Connecting to WiFi..");
   }
@@ -189,13 +184,15 @@ void connectToWifi(){
   Serial.println(ip);
 }
 
-
-void loop() {
+void loop()
+{
   mqttClient.loop();
 
   refreshLedStatus();
 
   // Controllo connessioni ed evenutale riconnessione
-  if(mqttClient.state() != MQTT_CONNECTED)  connectToBroker();
-  if(WiFi.status() != WL_CONNECTED) connectToWifi();
+  if (mqttClient.state() != MQTT_CONNECTED)
+    connectToBroker();
+  if (WiFi.status() != WL_CONNECTED)
+    connectToWifi();
 }
